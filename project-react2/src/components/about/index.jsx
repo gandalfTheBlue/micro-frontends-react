186b2a3f7665509c4cbb19338639e0563e4a9b95
andouillette-ys <yangshan0823@outlook.com>
2019-01-23 16:08:10 +0800

    update react 2 router

---
+++ project-react2/src/components/about/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>React project 2 About</div>;
+};
+
+export default About;
--- project-react2/src/components/app/index.jsx
+++ project-react2/src/components/app/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { AppRouter } from "../appRouter";
 import styles from "./app.module.css";
 
 const App = () => {
-  return <div className={styles.container}>test</div>;
+  return (
+    <div className={styles.container}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <AppRouter />
+    </div>
+  );
 };
 
 export default App;
